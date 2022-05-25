2ed3bcebce96d70f5f0232e46ec0c624053093e5
Ismael Gomes <ismaelgomesdasilva003@gmail.com>
2022-05-25 12:05:20 -0300

    exercicio5

---
+++ Exercicios/Exercicio5.js
@@ -0,0 +1,7 @@
+// Faça um programa que imprima na tela se um nome é igual ao outro nome digitado. Ex: João e João, imprime true. João e Maria, imprime false.
+
+let nameOne = 'Ismael'
+let nameTwo = 'Esmael'
+let result = nameOne === nameTwo
+
+console.log(result)
