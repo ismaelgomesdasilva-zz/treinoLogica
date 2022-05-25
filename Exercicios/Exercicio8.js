5142c1954f4f4baa56de4acc672fea6c67ff58c6
Ismael Gomes <ismaelgomesdasilva003@gmail.com>
2022-05-25 13:13:18 -0300

    Exercicio8

---
+++ Exercicios/Exercicio8.js
@@ -0,0 +1,36 @@
+// [ ] Criei 5 Objetos, neles devem conter os dados de 5 pessoas que você conhece. Mínimo 5 dados por pessoa.
+let ClienteOne = {
+    Name: 'Ismael',
+    Age: 21,
+    Address: 'Rua teresa Penoy',
+    AddressNumber: 160,
+    tags: ['academy', 'Front-end', 'Reactjs']
+}
+let ClienteTwo = {
+    Name: 'João',
+    Age: 24,
+    Address: 'Rua avaro severo',
+    AddressNumber: 23,
+    tags: ['academy', 'Front-end', 'Reactjs']
+}
+let ClienteThree = {
+    Name: 'Ismael',
+    Age: 21,
+    Address: 'Rua teresa Penoy',
+    AddressNumber: 160,
+    tags: ['academy', 'Front-end', 'Reactjs']
+}
+let ClienteFour = {
+    Name: 'Ismael',
+    Age: 21,
+    Address: 'Rua teresa Penoy',
+    AddressNumber: 160,
+    tags: ['academy', 'Front-end', 'Reactjs']
+}
+let ClienteFive = {
+    Name: 'Ismael',
+    Age: 21,
+    Address: 'Rua teresa Penoy',
+    AddressNumber: 160,
+    tags: ['academy', 'Front-end', 'Reactjs']
+}
